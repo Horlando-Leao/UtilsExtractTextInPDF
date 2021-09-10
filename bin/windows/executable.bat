c76290fce925811100fc3abdfa26557f7f62aacd
Horlando-Leao <horlandoleao8@gmail.com>
2021-09-10 14:46:48 -0300

    fixed .gitignore

--- bin/windows/executable.bat
+++
@@ -1,2 +0,0 @@
-START /WAIT java -jar ./bin/java/pdfbox-app-2.0.19.jar ExtractText cvs_database/42d477d7-4a3e-44bd-935e-e155b7ac50cd.pdf
-START /WAIT java -jar ./bin/java/pdfbox-app-2.0.19.jar ExtractText cvs_database/e090d3ab-be48-4e76-8a56-6c547fca40e1.pdf
