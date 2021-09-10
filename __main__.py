26a74bb57d4f4032550116884acb5e9c69edc520
Horlando-Leao <horlandoleao8@gmail.com>
2021-09-10 15:43:15 -0300

    add feature cli interactive user

--- __main__.py
+++ __main__.py
@@ -3,11 +3,30 @@ from cli.main.utils_tools import renames_all_files, extractPDF_text, clear_text_
 from cli.main.utils_tools import make_database_for_prodigy, get_number_of_files
 
 sys.argv[0] = '__main__.py'
+PATH_FILES_FIND_PDF = None
+PATH_FILES_TXT      = None
+PATH_SAVE_TXT_CLEAN = None
+PATH_SAVE_JSONL     = None
 
-PATH_FILES_FIND_PDF = str(sys.argv[1]) # path directory
-PATH_FILES_TXT      = str(sys.argv[2]) # Path find txt for clean garbage
-PATH_SAVE_TXT_CLEAN = str(sys.argv[3]) # path for save new txt clean
-PATH_SAVE_JSONL     = str(sys.argv[4]) # path save for new txt
+try:
+    PATH_FILES_FIND_PDF = str(sys.argv[1]) # path directory
+except IndexError:
+    PATH_FILES_FIND_PDF = input(str("Diretorio de arquivos de pdf para extrair e mudar o nome: "))
+
+try:
+    PATH_FILES_TXT      = str(sys.argv[2]) # Path find txt for clean garbage
+except IndexError:
+    PATH_FILES_TXT = input(str("Diretorio de arquivos de txt sujos: "))
+
+try:
+    PATH_SAVE_TXT_CLEAN = str(sys.argv[3]) # path for save new txt clean
+except IndexError:
+    PATH_SAVE_TXT_CLEAN = input(str("Diretorio para salvar arquivos os txt limpos: "))
+
+try:
+    PATH_SAVE_JSONL     = str(sys.argv[4]) # path save for new txt
+except IndexError:
+    PATH_SAVE_JSONL = input(str("Diretorio para salvar arquivo jsonl: "))
 
 
 if __name__ == '__main__' and get_number_of_files(PATH_FILES_FIND_PDF, 'pdf') > 0:
--- docs/executableWindons.ps1
+++ docs/executableWindons.ps1
@@ -4,4 +4,8 @@
 #    <Diretorio para salvar os arquivos txt pre processados>
 #    <Diretorio para salvar arquivos jsonl/nome do arquivo>
 
-python ..\__main__.py cvs_database cvs_database cvs_database/cvs_clean cvs_database/cvs_clean/database.jsonl
+
+
+python __main__.py cvs_database cvs_database cvs_database/cv_clean cvs_database/cv_clean/database.jsonl
+python .\cli\make_txt_for_jsonl.py ..\cv_aundes_txt\cv_clean\ ..\cv_aundes_txt\database.jsonl
+python .\cli\clear_text_txt.py ..\cv_aundes_txt ..\cv_aundes_txt\cv_clean
